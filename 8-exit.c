0f9fd7d7a72b7df6f5199daec7e38684fda9da09
Valritehachi <valritehachi@gmail.com>
2023-12-15 08:28:42 -0800

    edits

--- 8-exit.c
+++ 8-exit.c
@@ -9,20 +9,28 @@
  * @env : ...
  * Return: Always 0.
  */
-
 int internal_exit(int ac, char **av, char **env)
 {
-	int rc = 0;
+    int exit_status = 0;
 
-	env = env;
+    (void)ac;
+    (void)env;
 
-	if (ac > 1)
-	{
-		rc = _atoi(av[1]);
-	}
-	free_av(av);
-	free_env(env);
-	free_shell_alias();
-	exit(rc);
-	return (0);
+    if (av[1] != NULL)
+    {
+        exit_status = _atoi(av[1]);
+        if (exit_status == -1)
+        {
+            fprintf(stderr, "Error: Illegal number: %s\n", av[1]);
+            exit_status = 2;
+        }
+    }
+
+    free_av(av);
+    free_env(env);
+    free_shell_alias();
+
+    exit(exit_status);
+    return (0);
 }
+
